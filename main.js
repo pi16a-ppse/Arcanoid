dfd623346426672d00cdd37d6b23bad199cd9ff2
mikhaylovbogdan <mikhaylovbogdan98@mail.ua>
2019-04-28 15:49:12 +0300

    Output platform

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
 //Вывод фона на экран
 var game = {
+	width: 580,
+	height: 284,
     context: undefined,
+	bat: undefined,
 	art: {
-		background: undefined
+		background: undefined,
+		bat: undefined
 	},
 	
-	start: function(){
-        //апи для отрисовки
+	initialization(){
+	    //апи для отрисовки
         var canvas = document.getElementById("mycanvas"); 
         this.context = canvas.getContext("2d");
-		
-        this.art.background = new Image();
+	},
+	
+	loadingImage(){
+		for(var obj in this.art){
+		this.art[obj] = new Image();
 	    //Загружаем картинку
-	    this.art.background.src = "img/background.png"
+	    this.art[obj].src = "img/" + obj +".png";
+		}
+	},
+	
+	start: function(){		
+        this.initialization();
+		this.loadingImage();  
 		this.run();
 	  
 	},
 	
 	drawing : function(){
+	    //Очищаем выбранную прямоугольную область
+		this.context.clearRect(0,0, this.width, this.height);
 		//Отрисовываем картинку
 	    this.context.drawImage(this.art.background,0,0);
+		this.context.drawImage(this.art.bat,this.bat.x,this.bat.y);
 	},
 	
 	run: function(){
@@ -27,13 +43,18 @@ var game = {
 			    //Вывод изображения на экран
 	    window.requestAnimationFrame(function(){
 			//Зацикливаем перерисовку анимации
-			game.run();
+			game.run();  
 		});
 	}
 };
+    game.bat = { 
+        x: 250,
+		y: 258,
+	};
 
 // Запускать js код по факту загрузки html-страницы
-window.addEventListener("load",function(){
+    window.addEventListener("load",function(){
 // По факту загрузки страницы выполняем:
     game.start();
 });
+
