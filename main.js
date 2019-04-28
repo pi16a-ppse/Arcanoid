de1edc8eefefdcd3abf47fd910b90bde247e4d38
mikhaylovbogdan <mikhaylovbogdan98@mail.ua>
2019-04-28 13:02:53 +0300

    Refactoring and output baground

--- main.js
+++ main.js
@@ -6,24 +6,34 @@ var game = {
 	},
 	
 	start: function(){
-		//апи для отрисовки
-		var canvas = document.getElementById("mycanvas"); 
-		this.context = сanvas.getContext("2d");
+        //апи для отрисовки
+        var canvas = document.getElementById("mycanvas"); 
+        this.context = canvas.getContext("2d");
 		
-      this.art.background = new Image();
-	  //Загружаем картинку
-	  this.art.background.src = "img/background.png"
+        this.art.background = new Image();
+	    //Загружаем картинку
+	    this.art.background.src = "img/background.png"
+		this.run();
 	  
-	  //Отрисовываем картинку
-	  then.context.drawImage(this.art.background,0,0);
-	  
-	  //Вывод изображения на экран
-	  window.requestAnimationFrame()
+	},
+	
+	drawing : function(){
+		//Отрисовываем картинку
+	    this.context.drawImage(this.art.background,0,0);
+	},
+	
+	run: function(){
+		this.drawing();
+			    //Вывод изображения на экран
+	    window.requestAnimationFrame(function(){
+			//Зацикливаем перерисовку анимации
+			game.run();
+		});
 	}
 };
 
 // Запускать js код по факту загрузки html-страницы
 window.addEventListener("load",function(){
 // По факту загрузки страницы выполняем:
-game.start();
+    game.start();
 });
